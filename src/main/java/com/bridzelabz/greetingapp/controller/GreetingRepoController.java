5a59e608c8797e843b0db296d39cd6292e8019dc
ankita13-1997 <parhiankita@gmail.com>
2021-05-10 08:38:52 +0530

    [Modified]ankita:the code to find users by id from h2 database in the application

--- src/main/java/com/bridzelabz/greetingapp/controller/GreetingRepoController.java
+++ src/main/java/com/bridzelabz/greetingapp/controller/GreetingRepoController.java
@@ -11,41 +11,42 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 @RequestMapping("/greetingMsg")
 public class GreetingRepoController {
-    private static final String template ="Hello %s!";
-    private final AtomicLong counter =new AtomicLong();
+    private static final String template = "Hello %s!";
+    private final AtomicLong counter = new AtomicLong();
 
     @Autowired
     private IGreetingService greetingService;
 
 
     @GetMapping(value = {"", "/", "/home"})
-    public MessageGreet greeting(@RequestParam(value = "name",defaultValue = "World") String name){
-          User user =new User();
-          user.setFirstName(name);
-          return greetingService.addGreeting(user);
+    public MessageGreet greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        User user = new User();
+        user.setFirstName(name);
+        return greetingService.addGreeting(user);
 
     }
 
 
-    @GetMapping(value ={"/query"})
-    public MessageGreet greeting(@RequestParam(value = "firstName",defaultValue = "World") String firstName,
-                                 @RequestParam(value = "lastName",defaultValue = "World") String lastName){
-        User user =new User();
+    @GetMapping(value = {"/query"})
+    public MessageGreet greeting(@RequestParam(value = "firstName", defaultValue = "World") String firstName,
+                                 @RequestParam(value = "lastName", defaultValue = "World") String lastName) {
+        User user = new User();
         user.setFirstName(firstName);
         user.setLastName(lastName);
         return greetingService.addGreeting(user);
 
     }
 
-    @RequestMapping("/query1")
-    public String greeting(@RequestParam(value = "id") Long id){
+    @RequestMapping("/queryMsg")
+    public String greeting(@RequestParam(value = "id") Long id) {
         MessageGreet messageGreet = new MessageGreet();
         greetingService.deleteById(id);
-        return "the message of id " +id+ " is deleted from repo";
-
-
-
+        return "the message of id " + id + " is deleted from repo";
 
     }
 
+    @RequestMapping("/queryFind")
+    public MessageGreet greetingToFindById(@RequestParam(value = "id") Long id) {
+        return greetingService.findUsersById(id);
+    }
 }
